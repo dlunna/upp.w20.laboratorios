7307092fdbf80b0bfaf02d48990a3c39063ee86a
dlunna <dlunna@gmail.com>
2020-04-13 20:01:51 -0500

    Creación covi

---
+++ dotk/01header/carousel-principal/carouselimg.php
@@ -0,0 +1,35 @@
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="2" class="active"></li>
+  </ol>
+
+<!-- marca -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="first-slide center-block  mx-h-700" src="./images/slider/slider1a.jpg" alt="Biblioteca">
+    </div>
+
+    <div class="carousel-item ">
+      <img class="first-slide center-block  mx-h-700" src="./images/slider/slider2a.jpg" alt="Biblioteca">
+    </div>
+
+    <div class="carousel-item ">
+      <img class="first-slide center-block  mx-h-700" src="./images/slider/slider3.jpg" alt="Biblioteca">
+    </div>
+
+  </div>
+
+  <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+     <span class="sr-only">Previous</span>
+   </a>
+   <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+     <span class="carousel-control-next-icon" aria-hidden="true"></span>
+     <span class="sr-only">Next</span>
+   </a>
+
+
+</div>
